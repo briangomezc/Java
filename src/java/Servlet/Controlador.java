f4bcc6a0abc59d203b339f6858bb847c5a156c26
Brian <briangomez1994@gmail.com>
2020-06-13 20:47:47 -0400

    Los 3 Crud Completos

--- src/java/Servlet/Controlador.java
+++ src/java/Servlet/Controlador.java
@@ -6,6 +6,8 @@
 package Servlet;
 
 import Modelo.ClienteService;
+import Modelo.ModeloService;
+import Modelo.VehiculoService;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.RequestDispatcher;
@@ -24,52 +26,135 @@ public class Controlador extends HttpServlet {
     String edit = "edit.jsp";
     String index = "index.jsp";
     String delete = "detele.jsp";
+    String vehiculo = "Vehiculo.jsp";
+    String modelo = "Modelo.jsp";
+    String addM = "addM.jsp";
+    String addV = "addV.jsp";
+    String editM = "editM.jsp";
+    String editV = "editV.jsp";
+    String vehiculoN = "VehiculoN.jsp";
     String acceso = "";
     ClienteService cliente = new ClienteService();
+    ModeloService modeloC = new ModeloService();
+    VehiculoService vehiculoC = new VehiculoService();
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        String accion=request.getParameter("accion");
-        
-        if(accion.equals("add")){
+        String accion = request.getParameter("accion");
+
+        if (accion.equals("add")) {
             acceso = add;
         }
-        else if(accion.equals("index"))
-        {
+        if (accion.equals("modelo")) {
+            acceso = modelo;
+        } 
+        
+        if (accion.equals("addM")) {
+            acceso = addM;
+        }
+        
+        if (accion.equals("addV")) {
+            acceso = addV;
+        }
+        
+        if (accion.equals("vehiculoN")) {
+            acceso = vehiculoN;
+        }
+        else if (accion.equals("vehiculo")) {
+            acceso = vehiculo;
+   
+            
+        } else if (accion.equals("index")) {
+            acceso = index;
+        } else if (accion.equals("Guardar")) {
+            String Nombre = request.getParameter("txtNom");
+            String Apellido = request.getParameter("txtApe");
+            String Telefono = request.getParameter("txtTel");
+
+            cliente.agregar(Nombre, Apellido, Telefono);
             acceso = index;
         }
-        else if(accion.equals("Guardar")){
-        String Nombre = request.getParameter("txtNom");
-        String Apellido = request.getParameter("txtApe");
-        String Telefono = request.getParameter("txtTel");
         
-        cliente.agregar(Nombre, Apellido, Telefono);
-        acceso=index;
+        else if (accion.equals("Guardar Modelo")) {
+            String Nombre = request.getParameter("txtNom");
+            String Descripcion = request.getParameter("txtDes");
+
+            modeloC.agregarModelo(Nombre, Descripcion);
+            acceso = index;
         }
         
-        else if(accion.equals("editar")){
-        acceso = edit;
-        request.setAttribute("ID", request.getParameter("ID"));
+        else if (accion.equals("Guardar Vehiculo")) {
+            String Nombre = request.getParameter("txtNom");
+            String Descripcion = request.getParameter("txtDes");
+
+            vehiculoC.agregarVehiculo(Nombre, Descripcion);
+            acceso = index;
         }
-        
-        else if(accion.equals("Actualizar")){
+        else if (accion.equals("editar")) {
+            acceso = edit;
+            request.setAttribute("ID", request.getParameter("ID"));
+        }
+       
+        else if (accion.equals("Actualizar")) {
             String Nombre = request.getParameter("txtNom");
             String Apellido = request.getParameter("txtApe");
             String Telefono = request.getParameter("txtTel");
             int ID = Integer.parseInt(request.getParameter("txtID"));
-            
+
             cliente.actualizar(ID, Nombre, Apellido, Telefono);
-            acceso=index;
+            acceso = index;
+        } 
+        
+        else if (accion.equals("editarModelo")) {
+            acceso = editM;
+            request.setAttribute("ID", request.getParameter("ID"));
         }
-        else if(accion.equals("eliminar")){
-            int ID=Integer.parseInt(request.getParameter("ID"));
-            cliente.eliminar(ID);
-            acceso=index;
+             
+        else if (accion.equals("Actualizar Modelo")) {
+            String Nombre = request.getParameter("txtNom");
+            String Descripcion = request.getParameter("txtDes");
+            int ID = Integer.parseInt(request.getParameter("txtID"));
+
+            modeloC.editModelo(ID, Nombre, Descripcion);
+            acceso = index;
         }
         
+        else if (accion.equals("editarVehiculo")) {
+            acceso = editV;
+            request.setAttribute("ID", request.getParameter("ID"));
+        }
+        
+           else if (accion.equals("Actualizar Vehiculo")) {
+            String Nombre = request.getParameter("txtNom");
+            String Descripcion = request.getParameter("txtDes");
+            int ID = Integer.parseInt(request.getParameter("txtID"));
+
+            vehiculoC.editVehiculo(ID, Nombre, Descripcion);
+            acceso = index;
+        }
+        
+        else if (accion.equals("eliminar")) {
+            int ID = Integer.parseInt(request.getParameter("ID"));
+            cliente.eliminar(ID);
+            acceso = index;
+        }
+        
+        else if (accion.equals("eliminarM")) {
+            int ID = Integer.parseInt(request.getParameter("ID"));
+            modeloC.deleteModelo(ID);
+            acceso = modelo;
+        }
+        
+        else if (accion.equals("eliminarV")) {
+            int ID = Integer.parseInt(request.getParameter("ID"));
+            vehiculoC.deleteVehiculo(ID);
+            acceso = vehiculo;
+        }
+
         RequestDispatcher dispatcher = request.getRequestDispatcher(acceso);
         dispatcher.forward(request, response);
-        
+
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
